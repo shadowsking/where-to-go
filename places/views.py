bda93271cd20020c37166994b39b95a07212d850
shadowsking <shadowsking1295@gmail.com>
2024-01-08 08:25:25 +0800

    Add json api endpoint

--- places/views.py
+++ places/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Place
 
 
@@ -28,5 +29,26 @@ def show_start_page(request):
         "index.html",
         context={
             "places": features_collection
+        }
+    )
+
+
+def show_place_details(request, place_id):
+    place = get_object_or_404(Place, id=place_id)
+    images = list(map(lambda i: i.image.url, place.images.all()))
+    return JsonResponse(
+        {
+            "title": place.title,
+            "imgs": images,
+            "description_short": place.description_short,
+            "description_long": place.description_long,
+            "coordinates": {
+                "lat": place.lat,
+                "lng": place.lon
+            }
+        },
+        json_dumps_params={
+            "ensure_ascii": False,
+            "indent": 2
         }
     )
