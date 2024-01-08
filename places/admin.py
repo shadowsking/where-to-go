22bd11547627b2dc4dc78179721ce37921293d12
shadowsking <shadowsking1295@gmail.com>
2024-01-08 08:50:22 +0800

    Add tabular inline to place

--- places/admin.py
+++ places/admin.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from .models import Place, PlaceImage
 
 
+class PlaceImageInline(admin.TabularInline):
+    model = PlaceImage
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ('title', )
+    inlines = [
+        PlaceImageInline
+    ]
 
 
 @admin.register(PlaceImage)
