e3f95c3971bd5b34c226cff6368e059ed48ae315
shadowsking <shadowsking1295@gmail.com>
2024-01-08 23:27:51 +0800

    Add WYSIWYG editor

--- places/models.py
+++ places/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField('Наименование', max_length=200)
     description_short = models.TextField('Короткое описание', blank=True, null=True)
-    description_long = models.TextField('Полное описание', blank=True, null=True)
+    description_long = HTMLField('Полное описание', blank=True, null=True)
     lat = models.FloatField(verbose_name="Широта")
     lon = models.FloatField(verbose_name="Долгота")
 
