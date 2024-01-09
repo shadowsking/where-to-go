d87af99e970d5ac4cd8a2bd9baef5cbe3570c39e
shadowsking <shadowsking1295@gmail.com>
2024-01-09 17:17:30 +0800

    Created load place command

---
+++ places/management/commands/load_place.py
@@ -0,0 +1,41 @@
+import os.path
+
+import requests
+from django.core.files.base import ContentFile
+from django.core.management.base import BaseCommand
+from places.models import Place, PlaceImage
+
+
+class Command(BaseCommand):
+    help = "Command to load data from JSON to the DB"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "urls",
+            nargs="+",
+            type=str,
+            help="Ссылки на GEOJson"
+        )
+
+    def _upload_file(self, place, url):
+        response = requests.get(url)
+        response.raise_for_status()
+        return PlaceImage.objects.create(
+            place=place,
+            image=ContentFile(response.content, os.path.basename(url))
+        )
+
+    def handle(self, urls=None, *args, **options):
+        for url in urls or []:
+            response = requests.get(url)
+            response.raise_for_status()
+            place = response.json()
+            created_place, created = Place.objects.get_or_create(
+                title=place["title"],
+                description_short=place["description_short"],
+                description_long=place["description_long"],
+                lat=place["coordinates"]["lat"],
+                lon=place["coordinates"]["lng"],
+            )
+            for url in place.get("imgs"):
+                self._upload_file(created_place, url)
--- places/models.py
+++ places/models.py
@@ -3,18 +3,18 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField('Наименование', max_length=200)
+    title = models.CharField('Наименование', max_length=200, unique=True)
     description_short = models.TextField('Короткое описание', blank=True, null=True)
     description_long = HTMLField('Полное описание', blank=True, null=True)
-    lat = models.FloatField(verbose_name="Широта")
-    lon = models.FloatField(verbose_name="Долгота")
+    lat = models.FloatField(verbose_name='Широта')
+    lon = models.FloatField(verbose_name='Долгота')
 
     def __str__(self):
         return self.title
 
 
 class PlaceImage(models.Model):
-    image = models.ImageField(verbose_name='Изображение')
+    image = models.ImageField(verbose_name='Изображение', unique=True)
     position = models.PositiveIntegerField(
         default=0,
         verbose_name='Порядковый номер изображения'
